7df16d92f5157137b6dbf58e7f803ad1a87f3e6e
Blackbaud-StacyCarlos <stacy.carlos@blackbaud.com>
2018-02-20 12:55:52 -0500

    Added Observable import

--- src/app/public/src/modules/wrapper/anchor.service.ts
+++ src/app/public/src/modules/wrapper/anchor.service.ts
@@ -1,12 +1,12 @@
 import { Injectable} from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 import { StacheNavLink } from '../nav';
 
 @Injectable()
 export class StacheAnchorService {
 
     private anchor = new Subject<StacheNavLink>();
-    public anchorStream = this.anchor.asObservable();
+    public anchorStream: Observable<StacheNavLink> = this.anchor.asObservable();
 
     public addPageAnchor(anchor: StacheNavLink) {
         this.anchor.next(anchor);
